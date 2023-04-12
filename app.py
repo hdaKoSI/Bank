05d019ba596e172dd54c1620b73c9aed4b8a62f1
Tim Morgner <tim@timmorgner.de>
2023-04-12 14:15:05 +0200

    Docker experiments

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 import os
 import socket
+import time
 
+print("HEY")
+time.sleep(10)
 msgFromClient = "Hello UDP Server"
 
 bytesToSend = str.encode(msgFromClient)
 
-serverAddressPort = (os.environ.get("HOST"), os.environ.get("PORT"))
+serverAddressPort = (socket.gethostbyname(os.environ.get("HOST","127.0.0.1")), int(os.environ.get("PORT",12345)))
 
 bufferSize = 1024
 
 # Create a UDP socket at client side
 
+print("Started")
+
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+print("Socket created")
 # Send to server using created UDP socket
 
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+print("Message sent")
 
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
