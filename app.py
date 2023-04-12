09cc5e0d735de63793ee3b3f1a13b30e843fe5dd
Tim Morgner <tim@timmorgner.de>
2023-04-12 13:23:38 +0200

    Initial tests

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import socket
+
+msgFromClient = "Hello UDP Server"
+
+bytesToSend = str.encode(msgFromClient)
+
+serverAddressPort = (os.environ.get("HOST"), os.environ.get("PORT"))
+
+bufferSize = 1024
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
