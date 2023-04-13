e03a3d44f0b26b0fd413029f58454132cc77a0e0
Hus28 <hugo.schlott@web.de>
2023-04-13 13:07:54 +0200

    add some comments, fix prints

--- app.py
+++ app.py
@@ -2,30 +2,27 @@ import os
 import socket
 import time
 
-print("HEY")
+print("Programm Start")
 time.sleep(5)
 msgFromClient = "Hello UDP Server"
 
 bytesToSend = str.encode(msgFromClient)
-
 serverAddressPort = (socket.gethostbyname(os.environ.get("HOST","127.0.0.1")), int(os.environ.get("PORT",12345)))
 
 bufferSize = 1024
 
 # Create a UDP socket at client side
-
-print("Started")
-
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
 print("Socket created")
-print(serverAddressPort)
-# Send to server using created UDP socket
 
+print("Server Address Port: " + serverAddressPort)
+
+# Send to server using created UDP socket
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 print("Message sent")
 
+# Wait for message from Server
 while(True):
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
     msg = "Message from Server {}".format(msgFromServer[0])
